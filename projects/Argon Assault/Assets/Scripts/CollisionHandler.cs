050deb9ad9bad9ae24f44045ca47936d4eb75f02
PascalLiPooKim <paswei98@gmail.com>
2022-09-30 19:03:19 +0100

    Add Argon Assault project

---
+++ projects/Argon Assault/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // ok as long as this is the only script that load scenes
+
+public class CollisionHandler : MonoBehaviour
+{
+
+    [Tooltip("In seconds")] [SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")] [SerializeField] GameObject deathFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
+    }
+
+    private void StartDeathSequence()
+    {
+        print("Player dying");
+        SendMessage("OnPlayerDeath");
+    }
+
+    private void ReloadScene() // string referenced
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
